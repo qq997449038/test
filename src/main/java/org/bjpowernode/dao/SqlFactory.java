400a3103357a9f8e57be50f745a43da9cc03322a
weiwei <997449038@qq.com>
2021-06-20 20:00:29 +0800

    测试一下

--- src/main/java/org/bjpowernode/dao/SqlFactory.java
+++ src/main/java/org/bjpowernode/dao/SqlFactory.java
@@ -1,6 +1,7 @@
 package org.bjpowernode.dao;
 
 public class SqlFactory {
+    private  int a;
 
     public static Object getservice(Object object){
         return new InvocationHandlerimpl(object).Poxy();
